e904062eda01220e2d026979587953947412440e
Peter G. Andersen <peter.groenbaek@gmail.com>
2016-09-26 12:44:42 +0200

    Now removing duplicates by movie links

--- Datacleaning.m
+++ Datacleaning.m
@@ -5,3 +5,7 @@ clear all
 tblMovie=readtable('movie_metadata.csv','Format','%C%s%f%f%f%f%s%f%f%s%s%s%f%f%s%f%s%s%f%C%C%C%f%C%f%f%C%f');
 tblMissing = ismissing(tblMovie);
 tblMovie = tblMovie(~any(tblMissing,2),:);
+
+% Remove duplicates by checking movie links
+[~,ind] = unique(tblMovie(:,18));
+tblMovie = tblMovie(ind,:);
