6f16ee65faffff68fe91e862904957f6b34eaa4e
Halldor <halog11@student.sdu.dk>
2016-09-26 11:58:53 +0200

    example code for renaming headers in table, not used

--- Datacleaning.m
+++ Datacleaning.m
@@ -5,3 +5,4 @@ clear all
 tblMovie=readtable('movie_metadata.csv','Format','%C%s%f%f%f%f%s%f%f%s%s%s%f%f%s%f%s%s%f%C%C%C%f%C%f%f%C%f');
 tblMissing = ismissing(tblMovie);
 tblMovie = tblMovie(~any(tblMissing,2),:);
+% tbl.Properties.VariableNames={'Time' 'Gender' 'Height'};
