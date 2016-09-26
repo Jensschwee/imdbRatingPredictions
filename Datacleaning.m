cbad023fcb946024fa8ab21e40822dff5bd91aed
DESKTOP-11AA7AS\jens <Jensschwee>
2016-09-26 12:00:44 +0200

    data clean

--- Datacleaning.m
+++ Datacleaning.m
@@ -5,3 +5,5 @@ clear all
 tblMovie=readtable('movie_metadata.csv','Format','%C%s%f%f%f%f%s%f%f%s%s%s%f%f%s%f%s%s%f%C%C%C%f%C%f%f%C%f');
 tblMissing = ismissing(tblMovie);
 tblMovie = tblMovie(~any(tblMissing,2),:);
+clear tblMissing
+
