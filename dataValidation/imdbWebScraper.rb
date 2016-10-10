05d383f0f6675f05aadb3b4e1850690b39268eea
hlogason <halog11@student.sdu.dk>
2016-10-10 15:06:28 +0200

    web scraper rewritten completely, not tested but should work much better where it extract both budget and gross as well as checks if either are in dollars or not and adds them to a new csv file

--- dataValidation/imdbWebScraper.rb
+++ dataValidation/imdbWebScraper.rb
@@ -4,14 +4,21 @@ require 'json'
 require 'csv'
 
 $count
+$foreignCount
 
 def get_html
   $count = 0
+  $foreignCount = 0
   CSV.foreach('filtered_movie_set.csv', headers: true) do |row|
     url = row['movie_imdb_link']
     doc = Nokogiri::HTML(open(url))
     parse_html(doc, row)
+    sleep(Random.rand(20)+5)
   end
+
+  puts 'Total movies scraped: ' + $count.to_s
+  puts ''
+  puts 'Movies with foreign currency found: ' + $foreignCount.to_s
 end
 
 def parse_html(doc, row)
@@ -23,26 +30,40 @@ def parse_html(doc, row)
       File.open("grossText.txt", 'w') {|f| f.write(node.text) }
     end
   end
-  File.open('budgetText.txt').each_line.any?{|line|
-    line.gsub(/\s+/, "")
-    line.strip
-    line.gsub("Budget:", "")
-    line.gsub("(estimated)", "")
-    if line.include?('$')
-      row['budget'] = line
-    end
-  }
-  File.open('grossText.txt').each_line.any?{|line|
-    if line.include?('$')
-      row['gross'] = line
-    end
-  }
-  row['foreign_currency'] = 'false'
+
+  cleanFile('budgetText.txt')
+  cleanFile('grossText.txt')
+  budgetFile = File.open('budgetText.txt', 'r')
+  grossFile = File.open('grossText.txt', 'r')
+  budgetFileContent = File.read(budgetFile)
+  grossFileContent = File.read(grossFile)
   
+  row['budget'] = budgetFileContent
+  row['gross'] = grossFileContent
 
-
+  if budgetFileContent.include?('$') or grossFileContent.include?('$')
+    row['foreign_currency'] = 'false'
+  else if budgetFileContent.include?('USD') or grossFileContent.include?('USD')
+    row['foreign_currency'] = 'false'
+  else if budgetFileContent.include?('dollar') or grossFileContent.include?('dollar')
+    row['foreign_currency'] = 'false'
+  else if budgetFileContent.include?('dollars') or grossFileContent.include?('dollars')
+    row['foreign_currency'] = 'false'
+  else
+    $foreignCount += 1
+    row['foreign_currency'] = 'true'
+  end
+    
   $count += 1    
   puts $count
+end
+
+def cleanFile(file)
+  file = File.open(file, 'r')
+  oldText = File.read(file)
+  newText = oldText.gsub(/\s+/, "").gsub("Budget:", "").gsub("(estimated)", "").gsub("Gross:", "")
+  File.open(file, 'w') { |file| file.puts newText }
+  puts newText
 end
 
 get_html
