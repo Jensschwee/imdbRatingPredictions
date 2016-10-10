e9b1691f01242602486c2436dc1aad3e22a43700
petergreenbrook <peter.groenbaek@gmail.com>
2016-10-10 11:40:21 +0200

    Correct semicolon removal

--- dataValidation/removeSemiColon.rb
+++ dataValidation/removeSemiColon.rb
@@ -2,10 +2,5 @@ require 'csv'
 
 csvFile = File.open("foreign_movie_metadata.csv", 'r+')
 csvFile.each_line do |line|
-#  line.to_s.sub! ';' ''
-  line.strip()
-  line.chop!
-  #if line.end_with?(";")
-  
-  #end
+  line.gsub(";", "")
 end
