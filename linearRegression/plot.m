0a9449b45464ccfa5bee2fff5c5f984b77b145c1
hlogason <halog11@student.sdu.dk>
2016-10-31 10:53:21 +0100

    added a separate folder for linear regression and some empty files to show the intented structure

---
+++ linearRegression/plot.m
@@ -0,0 +1,7 @@
+function [ output_args ] = plot( input_args )
+%PLOT This function plots linear regression
+%   Detailed stuff..
+
+
+end
+
---
+++ linearRegression/run.m
@@ -0,0 +1,11 @@
+% Run script for Linear Regression
+
+
+
+% Gradient Descent
+
+
+% Eval
+
+
+% Plot
