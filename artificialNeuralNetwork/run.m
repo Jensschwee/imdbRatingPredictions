db8a58a03c0ec699d5423f41657849ad37d57233
Jensschwee <Jensschwee@msn.com>
2016-11-02 09:03:24 +0100

    ANN init workes

--- artificialNeuralNetwork/run.m
+++ artificialNeuralNetwork/run.m
@@ -1,28 +1,30 @@
+clc
+clear all
 tblMovieCleaned=readtable('../movie_metadata_cleaned.csv');
 
-%NumberOfReperts = 50;
+amountOfSampels = 500;
 
 % Gradient Descent
-X = table2array(tblMovieCleaned(:, 1)); %Color
-X = [X, table2array(tblMovieCleaned(:, 4))]; %Duration
+X = table2array(tblMovieCleaned(1:amountOfSampels, 1)); %Color
+X = [X, table2array(tblMovieCleaned(1:amountOfSampels, 4))]; %Duration
 %X = [X, table2array(tblMovieCleaned(:, 5))]; %director_facebook_likes
 %X = [X,table2array(tblMovieCleaned(:, 6))]; %actor_3_facebook_likes
 %X = [X,table2array(tblMovieCleaned(:, 8))]; %actor_1_facebook_likes
-X = [X,table2array(tblMovieCleaned(:, 14))]; %cast_total_facebook_likes
-X = [X, table2array(tblMovieCleaned(:, 226:244))]; %facenumber_in_poster
+X = [X,table2array(tblMovieCleaned(1:amountOfSampels, 14))]; %cast_total_facebook_likes
+X = [X, table2array(tblMovieCleaned(1:amountOfSampels, 226:244))]; %facenumber_in_poster
 %X = [X, table2array(tblMovieCleaned(:, 25))]; %actor_2_facebook_likes
-X = [X, table2array(tblMovieCleaned(:, 29:50))]; %genre
-X = [X, table2array(tblMovieCleaned(:, 51:84))]; %language
-X = [X, table2array(tblMovieCleaned(:, 85:127))]; %country
-X = [X, table2array(tblMovieCleaned(:, 128:133))]; %content_rating
-X = [X, table2array(tblMovieCleaned(:, 134:207))]; %title_year
-X = [X, table2array(tblMovieCleaned(:, 208:225))]; %aspect_ratio
-y = table2array(tblMovieCleaned(:, 245));
+X = [X, table2array(tblMovieCleaned(1:amountOfSampels, 29:50))]; %genre
+X = [X, table2array(tblMovieCleaned(1:amountOfSampels, 51:84))]; %language
+X = [X, table2array(tblMovieCleaned(1:amountOfSampels, 85:127))]; %country
+X = [X, table2array(tblMovieCleaned(1:amountOfSampels, 128:133))]; %content_rating
+X = [X, table2array(tblMovieCleaned(1:amountOfSampels, 134:207))]; %title_year
+X = [X, table2array(tblMovieCleaned(1:amountOfSampels, 208:225))]; %aspect_ratio
+y = table2array(tblMovieCleaned(1:amountOfSampels, 245));
 
 net = feedforwardnet([50]);%initialise network
 %net.divideFcn = 'dividetrain'; %this will only train on the whole dataset (no validation)
 net.divideFcn = 'divideind'; %divide data into training, validation and test sets
-[trainInd,valInd,testInd] = dividerand(length(X),0.7,0.15,0.15);%select data randomly
+[trainInd,valInd,testInd] = dividerand(size(X,1)    ,0.7,0.15,0.15);%select data randomly
 net.divideParam.trainInd = trainInd;
 net.divideParam.valInd = valInd;
 net.divideParam.testInd = testInd;
@@ -31,5 +33,25 @@ net.trainParam.goal = 0.00001;
 net.trainParam.max_fail = 5;
 
 t1 = tic;%initilise counter
-net = train(net,x,t);%train network
+net = train(net,X',y');%train network
 toc(t1)%compute elapsed time
+
+%testNetwork = sim(net,X');%test network on the whole data set
+MSE_tr=mean((y(trainInd)'-testNetwork(trainInd)).^2)%compute Mean Squared Error on the training set
+MSE_v=mean((y(valInd)'-testNetwork(valInd)).^2)%compute Mean Squared Error on the validation set
+MSE_t=mean((y(testInd)'-testNetwork(testInd)).^2)%compute Mean Squared Error on the test set
+
+clear t1
+clear testInd
+clear trainInd
+clear valInd
+clear MSE_tr
+clear MSE_v
+clear MSE_t
+clear amountOfSampels
+clear net
+clear X
+clear y
+
+
+
