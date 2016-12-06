2517581fdbe0a6c0e1395a44ac675e8cdf71953c
Jensschwee <Jensschwee@msn.com>
2016-12-06 14:51:54 +0100

    added epochs barchart errorbar

---
+++ linearRegression/epochsNeedBarchartErrorbar.m
@@ -0,0 +1,9 @@
+figure
+hold on
+bar(AlphaTuning(:,1),AlphaTuning(:,2))
+errorbar(AlphaTuning(:,1),AlphaTuning(:,2),AlphaTuning(:,3),'.')
+xlabel('Alpha')
+ylabel('r squared')
+title('Epochs need for Linear Regression')
+set(gca, 'Ylim', [0 350])
+set(gca,'fontsize',18)
