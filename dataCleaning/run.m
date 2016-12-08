a01297ed3c6f504128c0d24c526e2f4924cb9eab
Jensschwee <Jensschwee@msn.com>
2016-12-08 13:30:09 +0100

    New data cleaning now with IMDB score

--- dataCleaning/run.m
+++ dataCleaning/run.m
@@ -4,28 +4,40 @@ clc; clear all;
 tblMovie=readtable('../movie_metadata.csv','Format','%C%s%f%f%f%f%s%f%f%s%s%s%f%f%s%C%s%s%f%C%C%C%f%C%f%f%C%f');
 
 % Clean data.
-tblMovieCleaned = removeMissing(tblMovie);
+tblMovieCleaned = table(tblMovie.color,tblMovie.duration, tblMovie.genres, tblMovie.facenumber_in_poster, tblMovie.movie_imdb_link, tblMovie.language, tblMovie.country ,tblMovie.content_rating ,tblMovie.title_year, tblMovie.imdb_score, tblMovie.aspect_ratio, tblMovie.cast_total_facebook_likes, tblMovie.director_facebook_likes);
+tblMovieCleaned.Properties.VariableNames{'Var1'} = 'color';
+tblMovieCleaned.Properties.VariableNames{'Var2'} = 'duration';
+tblMovieCleaned.Properties.VariableNames{'Var3'} = 'genres';
+tblMovieCleaned.Properties.VariableNames{'Var4'} = 'facenumber_in_poster';
+tblMovieCleaned.Properties.VariableNames{'Var5'} = 'movie_imdb_link';
+tblMovieCleaned.Properties.VariableNames{'Var6'} = 'language';
+tblMovieCleaned.Properties.VariableNames{'Var7'} = 'country';
+tblMovieCleaned.Properties.VariableNames{'Var8'} = 'content_rating';
+tblMovieCleaned.Properties.VariableNames{'Var9'} = 'title_year';
+tblMovieCleaned.Properties.VariableNames{'Var10'} = 'imdb_score';
+tblMovieCleaned.Properties.VariableNames{'Var11'} = 'aspect_ratio';
+tblMovieCleaned.Properties.VariableNames{'Var12'} = 'cast_total_facebook_likes';
+tblMovieCleaned.Properties.VariableNames{'Var13'} = 'director_facebook_likes';
+
+tblMovieCleaned = removeMissing(tblMovieCleaned);
 tblMovieCleaned = removeDuplicates(tblMovieCleaned);
 tblMovieCleaned = standardizeCountries(tblMovieCleaned);
 tblMovieCleaned = standardizeContentRating(tblMovieCleaned);
-tblMovieCleaned = normalizeCategoricalDataFromColumnWithRegex(tblMovieCleaned, 10);
-tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 20);
-tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 21);
-tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 22);
-tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 24);
-tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 27);
-tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 16);
-tblMovieCleaned = removeIncorrectCurrencies(tblMovieCleaned, '../foreign_movie_links.csv');
+tblMovieCleaned = normalizeCategoricalDataFromColumnWithRegex(tblMovieCleaned, 3);
+tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 4);
+tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 6);
+tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 7);
+tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 8);
+tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 9);
+tblMovieCleaned = normalizeCategoricalDataFromColumn(tblMovieCleaned, 11);
+%tblMovieCleaned = removeIncorrectCurrencies(tblMovieCleaned, '../foreign_movie_links.csv');
 tblMovieCleaned = removeUnusedCategories(tblMovieCleaned);
-tblMovieCleaned = addRevenue(tblMovieCleaned);
+%tblMovieCleaned = addRevenue(tblMovieCleaned);
 tblMovieCleaned = removeOutliers(tblMovieCleaned);
 tblMovieCleaned = normalizeNumericalData(tblMovieCleaned);
 
 tblMovieCleaned.color = grp2idx(tblMovieCleaned.color);
 tblMovieCleaned.color = tblMovieCleaned.color - 1;
-tblManualTesting = tblMovieCleaned([2900,2999],:);
-tblMovieCleaned([2900,2999],:) = [];
 
 % Write cleaned table
 writetable(tblMovieCleaned, '../movie_metadata_cleaned.csv');
-writetable(tblManualTesting, '../movie_manualTesting_cleaned.csv');
--- images/plotsBetweenVaiables.m
+++ images/plotsBetweenVaiables.m
@@ -1,91 +1,93 @@
+tblMovieNormalized=readtable('../movie_metadata_cleaned.csv');
+
 %color 
 %TODO, make boxplot of both cats
-scatter(tblMovieNormalized.color, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.color, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
-xlabel('color')
-ylabel('revenue')
-title('color over revenue')
+xlabel('Color')
+ylabel('IMDB score')
+title('Color over IMDB score')
 print('color','-dpng');
 
 %num_critic_for_reviews
-scatter(tblMovieNormalized.num_critic_for_reviews, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.num_critic_for_reviews, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('num critic for reviews')
-ylabel('revenue')
-title('num critic for reviews over revenue')
+ylabel('IMDB score')
+title('num critic for reviews over IMDB score')
 print('num_critic_for_reviews','-dpng');
 
 %duration
-scatter(tblMovieNormalized.duration, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.duration, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('duration')
-ylabel('revenue')
-title('duration over revenue')
+ylabel('IMDB score')
+title('Duration over IMDB score')
 print('duration','-dpng');
 
 %director_facebook_likes
-scatter(tblMovieNormalized.director_facebook_likes, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.director_facebook_likes, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('director facebook likes')
-ylabel('revenue')
-title('director facebook likes over revenue')
+ylabel('IMDB score')
+title('director facebook likes over IMDB score')
 print('director_facebook_likes','-dpng');
 
 %actor_3_facebook_likes
-scatter(tblMovieNormalized.actor_3_facebook_likes, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.actor_3_facebook_likes, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('actor 3 facebook likes')
-ylabel('revenue')
-title('actor 3 facebook likes over revenue')
+ylabel('IMDB score')
+title('actor 3 facebook likes over IMDB score')
 print('actor_3_facebook_likes','-dpng');
 
 %actor_1_facebook_likes
-scatter(tblMovieNormalized.actor_1_facebook_likes, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.actor_1_facebook_likes, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('actor 1 facebook likes')
-ylabel('revenue')
-title('actor 1 facebook likes over revenue')
+ylabel('IMDB score')
+title('actor 1 facebook likes over IMDB score')
 print('actor_1_facebook_likes','-dpng');
 
 %cast_total_facebook_likes
-scatter(tblMovieNormalized.cast_total_facebook_likes, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.cast_total_facebook_likes, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('cast total facebook likes')
-ylabel('revenue')
-title('cast total facebook likes over revenue')
+ylabel('IMDB score')
+title('cast total facebook likes over IMDB score')
 print('cast_total_facebook_likes','-dpng');
 
 %actor_2_facebook_likes
-scatter(tblMovieNormalized.actor_2_facebook_likes, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.actor_2_facebook_likes, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('actor 2 facebook likes')
-ylabel('revenue')
-title('actor 2 facebook likes over revenue')
+ylabel('IMDB score')
+title('actor 2 facebook likes over IMDB score')
 print('actor_2_facebook_likes','-dpng');
 
 %facenumber_in_poster
 %TODO, make boxplot of both cats
-scatter(tblMovieNormalized.facenumber_in_poster, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.facenumber_in_poster, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('facenumber in poster')
-ylabel('revenue')
-title('facenumber in poster over revenue')
+ylabel('IMDB score')
+title('facenumber in poster over IMDB score')
 print('facenumber_in_poster','-dpng');
 
 %aspect_ratio
 %TODO, make boxplot of both cats
-scatter(tblMovieNormalized.aspect_ratio, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.aspect_ratio, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('aspect ratio')
-ylabel('revenue')
-title('aspect ratio over revenue')
+ylabel('IMDB score')
+title('aspect ratio over IMDB score')
 print('aspect_ratio','-dpng');
 
 %content_rating
 %TODO, make boxplot of both cats
-scatter(tblMovieNormalized.content_rating, tblMovieNormalized.revenue)
+scatter(tblMovieNormalized.content_rating, tblMovieNormalized.imdb_score)
 set(gca,'fontsize',18)
 xlabel('content rating')
-ylabel('revenue')
-title('content rating over revenue')
+ylabel('IMDB score')
+title('content rating over IMDB score')
 print('content_rating','-dpng');
