776a850cdb6c9798f24b889e93b1c86bbe006e53
Jensschwee <Jensschwee@msn.com>
2016-12-13 08:45:06 +0100

    new compare

--- Comparison/ComparisonBarchar.m
+++ Comparison/ComparisonBarchar.m
@@ -1,14 +1,15 @@
-rsquard = [0.5 0.2];
-CI = [0.1 0.15];
+rsquard = [0.4852 0.2];
+CI = [0.0471 0.15];
+labels = {'180' '90 55' '100 35 55'};
+XLabel = 'Artificial Neural Network Layer Settings';
 amount = 1:length(rsquard);
-labels = {'label0' 'label1'};
 
 figure
 hold on
 bar(amount,rsquard)
 colormap([0.6901960784313725,0.8784313725490196,0.9019607843137255])
 errorbar(amount,rsquard,CI,'.','color' , [1 0 0])
-xlabel('Model')
+xlabel(XLabel)
 ylabel('R Squared')
 set(gca, 'Ylim', [0 1])
 set(gca,...
