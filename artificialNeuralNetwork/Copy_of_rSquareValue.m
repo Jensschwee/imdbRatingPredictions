f76f67fe018740de854e6d9cbc7f224725bc7dfe
petergreenbrook <peter.groenbaek@gmail.com>
2016-12-14 20:29:48 +0100

    Bunch of stuff that probably shouldn't be here

---
+++ artificialNeuralNetwork/Copy_of_rSquareValue.m
@@ -0,0 +1,25 @@
+function [rSquare ] = Copy_of_rSquareValue(predict,realOut)
+%calc rSquare value
+%   Detailed explanation goes here
+    %meanRealVaule = mean(realVaule);
+    %rSquare = 1 - sum((realVaule -perdited).^2)/sum((realVaule - mean(realVaule)).^2);
+    
+    
+    % This formula return two values, we only use one of them. That might
+    % not be good. TODO
+    R = (cov(predict,realOut) / [(var(realOut)).^0.5 (var(realOut)).^0.5]);
+    rSquare = R(2).^2;
+    
+    R(1)
+    R(2)
+    
+    %rSquare=1-sum((realOut-predict).^2)/sum((realOut-mean(realOut)).^2);
+    
+    
+    
+    
+    %if rSquare < 0
+        %rSquare = 0;
+    %end
+end
+
